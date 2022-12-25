234a8af1b4af1450a7eba4c34f579460be08d4e0
M Naufal Ammar R <mnaufalammarr.15@gmail.com>
2022-12-25 12:44:42 +0700

    first Commit

---
+++ src/main/java/mii/mcc72/ams_client_app/AmsClientAppApplication.java
@@ -0,0 +1,13 @@
+package mii.mcc72.ams_client_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsClientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsClientAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/mii/mcc72/ams_client_app/config/AppSecurityConfig.java
@@ -0,0 +1,19 @@
+package mii.mcc72.ams_client_app.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest()
+                .permitAll();
+    }
+}
