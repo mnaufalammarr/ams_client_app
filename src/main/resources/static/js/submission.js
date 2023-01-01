816f7e8c602491f083708be0c353c38a399eeada
M Naufal Ammar R <mnaufalammarr.15@gmail.com>
2023-01-01 19:17:19 +0700

    new

--- src/main/resources/static/js/submission.js
+++ src/main/resources/static/js/submission.js
@@ -7,13 +7,14 @@ $('#tableSubmission').DataTable({
     columns: [{
         data: null,
         render: (data, type, row, meta) => {
-            console.log(data)
+            // console.log(data)
             return `<img src="http://localhost:8089/img/${data.image}" class="card-img-top" height="300px">   `
         }
     },{
         data: null,
         render: (data, type, row, meta) => {
             // console.log(data)
+            var status = data.approvedStatus == "PENDING_ADMIN" ? "PENDING" : data.approvedStatus;
             return `<div class=" m-3">
                         <div class="d-flex justify-content-between">
                             <span class="rounded font-weight-bold" ><h5 class="card-title ">${data.category.name}</h5></span>
@@ -21,6 +22,10 @@ $('#tableSubmission').DataTable({
                         </div>
                         <h2 class="card-title font-weight-bold">${data.name}</h2>
                         <h3 class="card-text font-weight-light">${data.description}</h3>
+                        <div class="d-flex justify-content-between">
+                        <h2 class="card-title font-weight-bold">Status : </h2>
+                        <h3 class="card-text font-weight-light">${status}</h3>
+                        </div>
                             <div class="d-flex justify-content-center "> <button type="button" class="btn btn-primary btn-lg mt-2" onclick="pindah('http://localhost:8089/v1/rentform/'+${data.id})">Detail</button></div>
                     </div>
 `
