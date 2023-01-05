b013bf1185e573ac85d1070ee0c88cdd639e3be0
Alif Andarta Al Falah <alifandarta26@gmail.com>
2023-01-05 16:20:07 +0700

    add error and confirm template

--- src/main/java/mii/mcc72/ams_client_app/models/dto/RegistrationDTO.java
+++ src/main/java/mii/mcc72/ams_client_app/models/dto/RegistrationDTO.java
@@ -9,6 +9,7 @@ public class RegistrationDTO {
     private String lastName;
     private String email;
     private String phoneNumber;
+    private int departmentId;
     private String username;
     private String password;
 }
